62fda7edcd7399a8a15da0d0f05db66b58eee310
Юлия Сенина <JuLKa9449@yandex.ru>
2022-06-29 11:26:23 +0300

    fourth commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,21 @@
-//const menuLink = document.querySelectorAll(".menu__link");
-const menuItem = Array.from(document.querySelectorAll(".menu__item"));
-const menuSub = document.querySelectorAll(".menu_sub");
+const menuSub = Array.from(document.querySelectorAll(".menu_sub"));
 
-for (let i = 0; i < menuItem.length; i++) {
-  menuItem[i].onclick = function() {
-    if(menuItem > menuSub) {
-      menuSub.className = "menu menu_sub menu_active";
-      console.log(menuSub.className);
-    if(menuSub.className = "menu menu_sub menu_active") {
-      menuSub.className = "menu menu_sub";
+for (let i = 0; i < menuSub.length; i++) {
+  const menuItem = menuSub[i].closest(".menu__item");
+  const menuLink = menuItem.querySelectorAll(".menu__link");
+
+  menuItem.onclick = function() {
+    if(menuSub[i] && menuLink != null) {
+      menuSub[i].className = "menu menu_sub menu_active";
+      console.log(menuSub[i].className);
+      let menuActive = menuItem.querySelector("ul").className.includes("menu_active");
+      console.log(menuActive);
+      if (menuActive === true) {
+        menuItem.onclick = function() {
+        menuSub[i].className = "menu menu_sub";
+      };
     };
-    return false;
+      return false;
     };
   };
 };
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,42 +2,26 @@ const sliderItem = Array.from(document.querySelectorAll(".slider__item"));
 const sliderArrowPrev = document.querySelector(".slider__arrow_prev");
 const sliderArrowNext = document.querySelector(".slider__arrow_next");
 
-function sliderDeactive (index) {
-  if (index === true) {
-    sliderItem[index].className = "slider__item";
-  };
-};
-
 sliderArrowNext.onclick = function() {
-  let index = sliderItem.findIndex((id) => {
-    if (id.className.includes("slider__item_active")) {
-      return id;
-    };
-  });
-
-  sliderDeactive;
-
-  index = index + 1;
+  let index = sliderItem.findIndex((id) => {id.className.includes("slider__item_active")});
+  sliderItem[index].className = "slider__item";
+  index + 1;
   if (index > (sliderItem.length - 1)) {
     index = 0;
   };
 
   sliderItem[index].className = "slider__item slider__item_active";
+  console.log(index);
 };
 
 sliderArrowPrev.onclick = function() {
-  let index = sliderItem.findIndex((id) => {
-    if (id.className.includes("slider__item_active")) {
-      return id;
-    };
-  });
-
-  sliderDeactive;
-
-  index = index --;
+  let index = sliderItem.findIndex((id) => {id.className.includes("slider__item_active")});
+  sliderItem[index].className = "slider__item";
+  index - 1;
   if (index <= 0) {
     index = sliderItem.length - 1;
   };
 
   sliderItem[index].className = "slider__item slider__item_active";
-}
+  console.log(index);
+};
