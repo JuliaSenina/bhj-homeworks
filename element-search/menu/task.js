bad7ff56ca65319de5d9abd20fcc2826a8311c6f
Юлия Сенина <JuLKa9449@yandex.ru>
2022-07-01 16:58:16 +0300

    sixth commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,19 +2,16 @@ const menuSub = Array.from(document.querySelectorAll(".menu_sub"));
 
 for (let i = 0; i < menuSub.length; i++) {
   const menuItem = menuSub[i].closest(".menu__item");
-  const menuLink = Array.from(menuItem.querySelectorAll(".menu__link"));
-
-  menuLink[i].onclick = function() {
-    menuSub[i].className = "menu menu_sub menu_active";
-    
-    const menuActive = document.querySelector(".menu_active");
-    console.log(menuActive);
-      if (menuActive === true) {
+  const menuLink = menuItem.querySelector(".menu__link");
+  
+  menuLink.onclick = function() { 
+    const menuActive = document.querySelector(".menu_active"); 
+      if (menuActive) {
         menuActive.className = "menu menu_sub"; 
-      };
+      };  
 
-      if (menuActive === menuSub[i]) {
-        return false;
-      };
+      if (menuActive !== menuSub[i]) {
+        menuSub[i].className = "menu menu_sub menu_active";
+      };      
   };
 };
