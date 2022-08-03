4dfd5fed0e8334ebe80fac667f0444928c419330
Юлия Сенина <JuLKa9449@yandex.ru>
2022-08-03 10:25:31 +0300

    third commit

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,32 +3,24 @@ const tasksList = document.getElementById("tasks__list");
 const tasksAdd = document.getElementById("tasks__add");
 
 function addTask() {
-  if (taskInput.value.trim() === "") {
-    alert("Задача не может быть пустой");
-  } else {
-    tasksList.insertAdjacentHTML("beforeend", `
-      <div class="task">
-      <div class="task__title">${taskInput.value}</div>
-      <a href="#" class="task__remove">&times;</a>
-      </div>
-    `);
+  tasksList.insertAdjacentHTML("beforeend", `
+    <div class= "task">
+    <div class="task__title">${taskInput.value}</div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>
+  `);
+  tasksList.querySelector(".task__remove").onclick = function name() {
+    this.closest(".task").remove();
   };
   taskInput.value = "";
-  taskRemove = document.querySelectorAll(".task__remove");
-  taskRemove[taskRemove.length - 1].addEventListener("click", removeElem);
-};
+}
 
-function addTaskOnEnter(item) {
-  if ((13 === item.keyCode)) {
-    addTask();
+tasksAdd.addEventListener("click", (elem) => {
+  elem.preventDefault();
+
+  if (taskInput.value.trim() !== "") {
+    addTask(taskInput.value);
+  } else {
+    alert("Задача не может быть пустой");
   };
-};
-
-taskInput.addEventListener("keyup", addTaskOnEnter);
-tasksAdd.addEventListener("click", (e) => {
-  addTask();
 });
-
-function removeElem(elem) {
-  elem.target.closest(".task").remove();
-};
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,28 +1,32 @@
 const hasTooltip = Array.from(document.querySelectorAll(".has-tooltip"));
+const tooltip = document.createElement("div");
+tooltip.classList.add("tooltip");
 
 hasTooltip.forEach(function(item) {
-    
-  item.onclick = function() {
-    const rect = item.getBoundingClientRect();
-    const tooltips = document.querySelector(".tooltip");
+  item.addEventListener("click", function(event) {
+    event.preventDefault();
 
-    if (item.nextElementSibling === null || item.nextElementSibling.classList.contains("tooltip") === false) {
+    const tooltipActive = document.querySelector(".tooltip_active");
+    const tooltips = Array.from(document.querySelectorAll(".tooltip"));
 
-      item.insertAdjacentHTML("afterend", `
-        <div class="tooltip" style="left: 0; top: 0"></div>
-      `);
-
-      const tooltip = item.nextElementSibling;
+    if (tooltips.includes((e) => e.textContent === item.getAttribute("title"))) {
+      tooltips.forEach((e) => {
+        if (e.textContent === item.getAttribute("title")) {
+          e.classList.toggle("tooltip_active");
+        };
+      });
+    } else if (tooltipActive) {
+      tooltipActive.classList.remove("tooltip_active");
+    } else {
       tooltip.textContent = item.getAttribute("title");
-      tooltip.classList.add("tooltip_active");
-      tooltip.style.top = (String(rect.bottom) + "px");
-      tooltip.style.left = (String(rect.left) + "px");
-      return false;
-    };
 
-    if (tooltips.classList.contains("tooltip_active")) {
-      tooltips.classList.remove("tooltip_active");
-      return false;
+      const itemLeft = item.getBoundingClientRect().left;
+      const itemTop = item.getBoundingClientRect().top;
+      tooltip.style.left = itemLeft + "px";
+      tooltip.style.top = (itemTop + 20) + "px";
+
+      item.appendChild(tooltip);
+      tooltip.classList.add("tooltip_active");
     }
-  };
-});
+  });
+})
